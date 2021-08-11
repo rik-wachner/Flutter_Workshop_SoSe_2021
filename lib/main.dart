136e22c174cb6f45b8e6d15e4e65bfa7138d5fde
Rik Wachner <2150438@stud.hs-mannheim.de>
2021-08-11 15:12:46 +0200

    Checkpoint 03 - Hello Flutter Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyCoreApp());
 
 class MyCoreApp extends StatelessWidget {
+
+  void sayHello() {
+    print('Hello Flutter?!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +15,19 @@ class MyCoreApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Hello Flutter?!'),
         ),
-        body: Text('Hello Flutter?!'),
+        body: Column(
+          children: <Widget>[
+            Text('Buttons:'),
+            ElevatedButton(onPressed: null, child: Text('Variante A')),
+            RaisedButton(onPressed: sayHello, child: Text('Variante B')),
+            ElevatedButton(
+                onPressed: () {
+                  //TODO
+                  print("Ich möchte nicht nur Hello Flutter sagen :(");
+                },
+                child: Text('Variante C'))
+          ],
+        ),
       ),
     );
   }
