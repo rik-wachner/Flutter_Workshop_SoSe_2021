4099e65d4d122738d3099a235c36735d2b2c8c98
Rik Wachner <2150438@stud.hs-mannheim.de>
2021-08-16 21:13:51 +0200

    Checkpoint 04 - Hello Flutter State

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,48 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyCoreApp());
 
 class MyCoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyStatePage());
+  }
+}
+
+class MyStatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyStatePageState();
+  }
+}
+
+class _MyStatePageState extends State<MyStatePage> {
+  var _titleText = "Button nicht gedrückt";
 
   void sayHello() {
     print('Hello Flutter?!');
+    setState(() {
+      _titleText = "Button gedrückt";
+    });
+    print(_titleText);
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter?!'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Text('Buttons:'),
-            ElevatedButton(onPressed: null, child: Text('Variante A')),
-            RaisedButton(onPressed: sayHello, child: Text('Variante B')),
-            ElevatedButton(
-                onPressed: () {
-                  //TODO
-                  print("Ich möchte nicht nur Hello Flutter sagen :(");
-                },
-                child: Text('Variante C'))
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Flutter?! - $_titleText'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text('Buttons:'),
+          ElevatedButton(onPressed: null, child: Text('Variante A')),
+          RaisedButton(onPressed: sayHello, child: Text('Variante B')),
+          ElevatedButton(
+              onPressed: () {
+                //TODO
+                print("Ich möchte nicht nur Hello Flutter sagen :(");
+              },
+              child: Text('Variante C'))
+        ],
       ),
     );
   }
