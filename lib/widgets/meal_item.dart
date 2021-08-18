e82b87edc5ae2ad265106112fd7ecdd4ae912e83
Rik Wachner <2150438@stud.hs-mannheim.de>
2021-08-18 17:44:29 +0200

    Checkpoint 07 - Tune Meals Screen

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import '../models/meal.dart';
 
-// Schritt 1: Stateless Widget
 class MealItem extends StatelessWidget {
-  // Schritt 2: MealItem benutzt was für Attribute des Meals ?
-  //  --> Alle außer ingredients und step!
-  // Propertys auflisten schonmal
   final String id;
   final String title;
   final Complexity complexity; // Import '../models/meal.dart'
   final int kcal;
   final String imageUrl;
   final int duration;
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Sehr Einfach';
+        break;
+      case Complexity.Challenging:
+        return 'Schwer';
+        break;
+      case Complexity.Hard:
+        return 'Sehr Schwer';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
 
   MealItem(
       {@required this.id,
@@ -29,15 +41,48 @@ class MealItem extends StatelessWidget {
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
         elevation: 4,
         margin: EdgeInsets.all(10),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(15), topRight: Radius.circular(15)),
-          child: Image.network(
-            imageUrl,
-            height: 250,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
+        child: Column(
+          children: <Widget>[
+            Stack(children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15)),
+                child: Image.network(
+                  imageUrl,
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  width: 300,
+                  color: Colors.black45,
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  child: Text(
+                    title,
+                    style: TextStyle(fontSize: 26, color: Colors.white),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              ),
+            ]),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text('$duration min'),
+                  Text('$complexityText'),
+                  Text('$kcal kcal'),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
